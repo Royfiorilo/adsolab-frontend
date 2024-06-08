b5f8e3babfc4f818850585f23e089876d59ea1ed
andivisciglio <avisciglio@fi.uba.ar>
2024-06-08 16:37:24 -0300

    feat: model configuration layout

--- src/app/investigation/investigation.component.ts
+++ src/app/investigation/investigation.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+class Model {
+}
+
 @Component({
   selector: 'app-investigation',
   templateUrl: './investigation.component.html',
@@ -10,16 +13,38 @@ export class InvestigationComponent {
   selectedFile: File | null = null;
   progressBarValue: number = 0;
   stepId: number = 0;
-  models: { name: string, description: string }[] = [{
+  models: { name: string, description: string, params: number }[] = [{
     name: "Langmuir",
-    description: "The Langmuir adsorption model explains adsorption by assuming an adsorbate behaves as an ideal gas at isothermal conditions. According to the model, adsorption and desorption are reversible processes."
+    description: "The Langmuir adsorption model explains adsorption by assuming an adsorbate behaves as an ideal gas at isothermal conditions. According to the model, adsorption and desorption are reversible processes.",
+    params: 4
+
   },{
     name: "Freundlich",
-    description: "The Freundlich equation or Freundlich adsorption isotherm, an adsorption isotherm, is an empirical relationship between the quantity of a gas adsorbed into a solid surface and the gas pressure. The same relationship is also applicable for the concentration of a solute adsorbed onto the surface of a solid and the concentration of the solute in the liquid phase."
-  }]
+    description: "The Freundlich equation or Freundlich adsorption isotherm, an adsorption isotherm, is an empirical relationship between the quantity of a gas adsorbed into a solid surface and the gas pressure. The same relationship is also applicable for the concentration of a solute adsorbed onto the surface of a solid and the concentration of the solute in the liquid phase.",
+    params: 2
+  }];
+  sheetData: any[] = [];
+  sheetHeaders: string[] = [];
+  selectedModels: string[] = [];
+  modelSelections: { [key: string]: number } = {};
 
   onFileSelected(event: any) {
     this.selectedFile = event.target.files[0];
+  }
+  addModel(model: string){
+    this.selectedModels.push(model);
+    this.modelSelections[model] = 1;
+  }
+
+
+  onModelSelected(model: string) {
+    this.selectedModels.includes(model) ? this.selectedModels.splice(this.selectedModels.indexOf(model),1) : this.addModel(model);
+    console.log(this.selectedModels);
+  }
+
+  getParamsArray(modelName: string): number[] {
+    let model  = this.models.find(m => m.name === modelName);
+    return model === undefined ? [] : Array(model.params).fill(0).map((x, i) => i) ;
   }
 
   onUpload() {
