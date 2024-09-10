b92b006c3e43f76bd8890fb61a7c2e57298732c2
Federico Rossini <federico.rossini@kavak.com>
2024-09-10 20:02:33 -0300

    chore: fix envs

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
 import {IEnvironment} from "./interface";
 
 export const environment: IEnvironment = {
-  // backendBaseUrl: 'https://adsolab-back-hz11.onrender.com/'
-  backendBaseUrl: 'http://127.0.0.1:5000'
+  backendBaseUrl: 'https://adsolab-back-hz11.onrender.com/'
 };
