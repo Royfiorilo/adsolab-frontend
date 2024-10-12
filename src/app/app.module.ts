ea871ff89b95e6ba7c6539ce4774a9ab8b54af6d
andivisciglio <avisciglio@fi.uba.ar>
2024-10-12 13:57:44 -0300

    data visualizer component for data upload, and linearization page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,6 +27,7 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {LoadingComponent} from './components/loading/loading.component';
 import {MatProgressSpinner} from "@angular/material/progress-spinner";
+import { DataVisualizerComponent } from './components/data-visualizer/data-visualizer.component';
 
 //Plotly.register(locale)
 
@@ -45,7 +46,8 @@ PlotlyModule.plotlyjs = Plotly;
     ProgressBarComponent,
     FileUploadComponent,
     MainPageComponent,
-    LoadingComponent
+    LoadingComponent,
+    DataVisualizerComponent
   ],
   imports: [
     BrowserModule,
