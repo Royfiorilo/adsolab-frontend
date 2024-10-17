48fd330461780b8bf9d0299e781565ce211d6438
Federico Rossini <federico.rossini@kavak.com>
2024-10-17 20:06:18 -0300

    feat: all texts and upload file refactor

--- src/app/components/file-upload/file-upload.component.ts
+++ src/app/components/file-upload/file-upload.component.ts
@@ -1,6 +1,8 @@
 import {Component, EventEmitter, Output} from '@angular/core';
 import * as XLSX from 'xlsx';
-import {DataSample} from "../data-selector/data-sample";
+import {DataSample, InvalidFileReason} from "../data-selector/data-sample";
+import {faCircleCheck, faFileUpload, faXmarkCircle} from '@fortawesome/free-solid-svg-icons';
+import {TranslateService} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-file-upload',
@@ -12,6 +14,13 @@ export class FileUploadComponent {
   errorMessage: string | null = null;
   successMessage: string | null = null;
   @Output() onDataSampleUploaded: EventEmitter<DataSample> = new EventEmitter();
+  protected faUpload = faFileUpload;
+  protected faCircleCheck = faCircleCheck;
+  protected faXmarkCircle = faXmarkCircle;
+  protected uploadedFile: { name?: string, valid?: boolean, reason?: InvalidFileReason } = {};
+
+  constructor(private translate: TranslateService) {
+  }
 
   onDragOver(event: DragEvent) {
     event.preventDefault();
@@ -45,10 +54,12 @@ export class FileUploadComponent {
   handleFile(file: File) {
     this.errorMessage = null;
     this.successMessage = null;
+    this.uploadedFile = {name: file.name};
 
     const allowedTypes = ['application/vnd.ms-excel', 'text/csv', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'];
     if (!allowedTypes.includes(file.type)) {
-      this.errorMessage = 'Solo se admiten archivos en formato Excel o CSV.';
+      this.uploadedFile.valid = false;
+      this.uploadedFile.reason = InvalidFileReason.INVALID_FILE_TYPE;
       return;
     }
 
@@ -66,20 +77,20 @@ export class FileUploadComponent {
         if (isCSV) {
           content = content as string;
         } else {
-          const workbook = XLSX.read(content, { type: 'binary' });
+          const workbook = XLSX.read(content, {type: 'binary'});
           const sheetName = workbook.SheetNames[0];
           const sheet = workbook.Sheets[sheetName];
           content = XLSX.utils.sheet_to_csv(sheet);
         }
         this.onDataSampleUploaded.emit(this.validateCSVContent(content));
       } catch (e: any) {
-        this.errorMessage = e.message;
+        this.setInvalidUploadedFile(InvalidFileReason.READ_ERROR);
       }
 
     };
 
     reader.onerror = () => {
-      this.errorMessage = 'Error en la lectura del archivo.';
+      this.setInvalidUploadedFile(InvalidFileReason.READ_ERROR);
     };
 
     if (isCSV) {
@@ -90,7 +101,12 @@ export class FileUploadComponent {
 
   }
 
-  validateCSVContent(content: string): DataSample {
+  setInvalidUploadedFile(reason: InvalidFileReason) {
+    this.uploadedFile.valid = false;
+    this.uploadedFile.reason = reason;
+  }
+
+  validateCSVContent(content: string): DataSample | undefined {
     const lines = content.split('\n');
     const dataSample: DataSample = {
       description: undefined, sample_id: undefined, label: "",
@@ -100,15 +116,17 @@ export class FileUploadComponent {
 
     for (const line of lines) {
       const columns = line.split(',');
-      if (columns.length !== 2){
-        throw new Error('Formato invalido. El archivo debe tener exactamente dos columnas.');
+      if (columns.length !== 2) {
+        this.setInvalidUploadedFile(InvalidFileReason.INVALID_FILE_STRUCTURE)
+        return undefined;
       } else if (columns[0] === "" || columns[1] === "" || isNaN(Number(columns[0])) || isNaN(Number(columns[1]))) {
-        throw new Error('Formato invalido. El archivo debe tener exclusivamente valores numericos.');
+        this.setInvalidUploadedFile(InvalidFileReason.INVALID_DATA)
+        return undefined;
       }
       dataSample.ce.push(Number(columns[0]))
       dataSample.qe.push(Number(columns[1]))
     }
-    this.successMessage = 'Archivo valido!';
+    this.uploadedFile.valid = true;
     return dataSample;
   }
 }
