ea8b7a5320d291b9887ed51e336d7617ef57d74e
franciscoL0pez <franlopez.2001@hotmail.com>
2026-05-31 22:50:26 -0300

    modify navbar

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -4,9 +4,11 @@ import {EditUserComponent} from "../edit-user/edit-user.component";
 import {SnackBarComponent} from "../snack-bar/snack-bar.component";
 import {MatDialog} from "@angular/material/dialog";
 import {MatSnackBar} from "@angular/material/snack-bar";
+import {MatSidenav} from "@angular/material/sidenav";
 import {BreakpointObserver, Breakpoints} from "@angular/cdk/layout";
 import {map} from "rxjs/operators";
 import {Observable, shareReplay} from "rxjs";
+import {faClock, faFlaskVial, faGauge, faHistory, faUsers} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-navigation',
@@ -16,6 +18,14 @@ import {Observable, shareReplay} from "rxjs";
 export class NavigationComponent {
 
   isMobile$: Observable<boolean>;
+  private isMobile = false;
+
+  // Sidebar navigation icons
+  protected readonly faGauge = faGauge;
+  protected readonly faFlaskVial = faFlaskVial;
+  protected readonly faClock = faClock;
+  protected readonly faHistory = faHistory;
+  protected readonly faUsers = faUsers;
 
   constructor(protected authService: AuthService, private dialog: MatDialog, private _snackBar: MatSnackBar, breakpointObserver: BreakpointObserver) {
     this.isMobile$ = breakpointObserver.observe([Breakpoints.XSmall, Breakpoints.Small])
@@ -23,10 +33,18 @@ export class NavigationComponent {
         map(result => result.matches),
         shareReplay()
       );
+    this.isMobile$.subscribe(isMobile => this.isMobile = isMobile);
   }
 
   logout() {
     this.authService.logout();
+  }
+
+  // On desktop the sidebar is persistent (side mode); only close it on mobile overlays.
+  closeOnMobile(snav: MatSidenav) {
+    if (this.isMobile) {
+      snav.close();
+    }
   }
 
   isAdmin(): boolean {
