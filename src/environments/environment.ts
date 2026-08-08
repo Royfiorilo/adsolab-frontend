83ffdaafbe3ee5cff0b7fc96ee406415febf1553
FrancoCorn <91103071+FrancoCorn@users.noreply.github.com>
2026-08-07 23:47:54 -0300

    Update backendBaseUrl in environment configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 import {IEnvironment} from "./interface";
 
 export const environment: IEnvironment = {
-  backendBaseUrl: 'https://adsolab-back-hz11.onrender.com',
+  backendBaseUrl: 'https://adsolab-backend.onrender.com',
   authEnabled: false
 };
